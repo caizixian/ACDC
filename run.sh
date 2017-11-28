49dd4c9439234a2aaca647f91f0d49a750810b76
Zixian Cai <u5937495@anu.edu.au>
2017-11-28 14:27:31 +1100

    Update the run script

--- run.sh
+++ run.sh
@@ -12,9 +12,7 @@ BUILDTYPE=Release make
 cp $MMTK_PATH/libmmtk.so ./allocators/libmmtk.so
 cp $MMTK_PATH/libmmtkc.so ./allocators/libmmtkc.so
 export LD_LIBRARY_PATH=./allocators
-LD_PRELPOAD=./allocators/libmmtk.so ./out/Release/acdc -P mmtk
-rm ./allocators/libmmtk.so
-mv ./allocators/libmmtkc.so ./allocators/libmmtk.so
-LD_PRELPOAD=./allocators/libmmtk.so ./out/Release/acdc -P mmtk
-LD_PRELPOAD=./allocators/libjemalloc.so ./out/Release/acdc -P jemalloc
-LD_PRELPOAD=./allocators/libscalloc.so ./out/Release/acdc -P scalloc
+LD_PRELOAD=./allocators/libmmtk.so ./out/Release/acdc -P mmtk
+LD_PRELOAD=./allocators/libmmtkc.so ./out/Release/acdc -P mmtkc
+LD_PRELOAD=./allocators/libjemalloc.so ./out/Release/acdc -P jemalloc
+LD_PRELOAD=./allocators/libhoard.so ./out/Release/acdc -P hoard
