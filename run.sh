41e7116d59546c5849edc5f18088b2beab575e80
Zixian Cai <u5937495@anu.edu.au>
2017-11-28 16:59:57 +1100

    Add parameters from the scalloc manual

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ BUILDTYPE=Release make
 cp $MMTK_PATH/libmmtk.so ./allocators/libmmtk.so
 cp $MMTK_PATH/libmmtkc.so ./allocators/libmmtkc.so
 export LD_LIBRARY_PATH=./allocators
-LD_PRELOAD=./allocators/libmmtk.so ./out/Release/acdc -P mmtk
-LD_PRELOAD=./allocators/libmmtkc.so ./out/Release/acdc -P mmtkc
-LD_PRELOAD=./allocators/libjemalloc.so ./out/Release/acdc -P jemalloc
-LD_PRELOAD=./allocators/libhoard.so ./out/Release/acdc -P hoard
+LD_PRELOAD=./allocators/libmmtk.so ./out/Release/acdc -P mmtk -a -s 4 -S 9 -d 30 -l 1 -L 5 -t 1000000
+LD_PRELOAD=./allocators/libmmtkc.so ./out/Release/acdc -P mmtkc -a -s 4 -S 9 -d 30 -l 1 -L 5 -t 1000000
+LD_PRELOAD=./allocators/libjemalloc.so ./out/Release/acdc -P jemalloc -a -s 4 -S 9 -d 30 -l 1 -L 5 -t 1000000
+LD_PRELOAD=./allocators/libhoard.so ./out/Release/acdc -P hoard -a -s 4 -S 9 -d 30 -l 1 -L 5 -t 1000000
