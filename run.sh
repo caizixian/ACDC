a7eba20e0ae819538e99611e133b6acddf58a2da
Zixian Cai <u5937495@anu.edu.au>
2017-11-28 13:50:39 +1100

    Really using the C version

--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ cp $MMTK_PATH/libmmtk.so ./allocators/libmmtk.so
 cp $MMTK_PATH/libmmtkc.so ./allocators/libmmtkc.so
 export LD_LIBRARY_PATH=./allocators
 LD_PRELPOAD=./allocators/libmmtk.so ./out/Release/acdc -P mmtk
-LD_PRELPOAD=./allocators/libmmtkc.so ./out/Release/acdc -P mmtkc
+rm ./allocators/libmmtk.so
+mv ./allocators/libmmtkc.so ./allocators/libmmtk.so
+LD_PRELPOAD=./allocators/libmmtk.so ./out/Release/acdc -P mmtk
 LD_PRELPOAD=./allocators/libjemalloc.so ./out/Release/acdc -P jemalloc
 LD_PRELPOAD=./allocators/libscalloc.so ./out/Release/acdc -P scalloc
